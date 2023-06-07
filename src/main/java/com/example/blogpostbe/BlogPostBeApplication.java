d064701105f2fd810fd7784b012973cbc9acd01f
Isaak Morales <ice.isaak97@gmail.com>
2023-06-06 23:39:01 -0400

    first commit

---
+++ src/main/java/com/example/blogpostbe/BlogPostBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogpostbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPostBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blogpostbe/BlogPostBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogpostbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
